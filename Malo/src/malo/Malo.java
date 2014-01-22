5f003021c20641b866a2357daf7e51eaaf08167b
Guillermo Perez de la Torre <forcelottus@gmail.com>
2014-01-21 21:28:50 -0600

    Le cambie el print, YAY!

--- Malo/src/malo/Malo.java
+++ Malo/src/malo/Malo.java
@@ -16,7 +16,7 @@ public class Malo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Mi primer proyecto que no es spring :(");
+        System.out.println("Mi primer proyecto que no es spring :'( aaawww dude");
     }
     
 }
